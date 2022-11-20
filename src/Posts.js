71d6523221b65273d412fcd30cbd376acf85a121
Luis Bernardo <102603174+stonasss@users.noreply.github.com>
2022-11-20 17:13:45 -0300

    funcionalidades add: salvar & curtir post

--- src/Posts.js
+++ src/Posts.js
@@ -1,8 +1,25 @@
+import React, {useState} from "react";
+
 export default function Posts(){
     const posts = [
-        {perfil: "assets/img/meowed.svg", usuario: "meowed", imagem: "assets/img/gato-telefone.svg", smallimg: "assets/img/respondeai.svg", amigo: "respondeai", curtidas: "outras 101.523 pessoas"},
-        {perfil: "assets/img/barked.svg", usuario: "barked", imagem: "assets/img/dog.svg", smallimg: "assets/img/adorable_animals.svg", amigo: "adorable_animals", curtidas: "outras 99.159 pessoas"}
+        {
+        perfil: "assets/img/meowed.svg", 
+        usuario: "meowed", 
+        imagem: "assets/img/gato-telefone.svg", 
+        smallimg: "assets/img/respondeai.svg", 
+        amigo: "respondeai", 
+        curtidas: 101523
+    },
+
+        {perfil: "assets/img/barked.svg", 
+        usuario: "barked", 
+        imagem: "assets/img/dog.svg", 
+        smallimg: "assets/img/adorable_animals.svg", 
+        amigo: "adorable_animals", 
+        curtidas: 99159
+    }
     ]
+    
     return (
         <div class="posts">
             {posts.map((post) => <Post perfil={post.perfil} usuario={post.usuario} imagem={post.imagem} smallimg={post.smallimg} amigo={post.amigo} curtidas={post.curtidas}/>)}
@@ -11,6 +28,35 @@ export default function Posts(){
 }
 
 function Post(props){
+    const [bookmarkClicked, setbookmarkClicked] = useState("bookmark-outline")
+    const [heartClicked, setheartClicked] = useState("heart-outline")
+    const [totalLikes, settotalLikes] = useState(Number(props.curtidas))
+
+    function savePost(){
+        if (bookmarkClicked == "bookmark-outline"){
+            setbookmarkClicked("bookmark-sharp")
+        } else if (bookmarkClicked == "bookmark-sharp") {
+            setbookmarkClicked("bookmark-outline")
+        }
+    }
+
+    function likePost(){
+        if (heartClicked == "heart-outline"){
+            setheartClicked("heart")
+            settotalLikes(props.curtidas + 1)
+        } else if (heartClicked == "heart") {
+            setheartClicked("heart-outline")
+            settotalLikes(totalLikes - 1)
+        }
+    }
+
+    function likeImg(){
+        if (heartClicked == "heart-outline"){
+            setheartClicked("heart")
+            settotalLikes(props.curtidas + 1)
+        }
+    }
+
     return (
         <div data-test="post" class="post">
             <div class="topo">
@@ -24,25 +70,25 @@ function Post(props){
             </div>
 
             <div class="conteudo">
-                <img data-test="post-image" src={props.imagem} />
+                <img data-test="post-image" onClick={likeImg} src={props.imagem} />
             </div>
 
             <div class="fundo">
                 <div class="acoes">
                     <div>
-                        <ion-icon data-test="like-post" name="heart-outline"></ion-icon>
+                        <ion-icon data-test="like-post" onClick={likePost} name={heartClicked}></ion-icon>
                         <ion-icon name="chatbubble-outline"></ion-icon>
                         <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
                     <div>
-                        <ion-icon data-test="save-post" name="bookmark-outline"></ion-icon>
+                        <ion-icon data-test="save-post" onClick={savePost} name={bookmarkClicked}></ion-icon>
                     </div>
                 </div>
             
                 <div class="curtidas">
                     <img src={props.smallimg} />
                     <div class="texto">
-                        Curtido por <strong>{props.amigo}</strong> e <strong data-test="likes-number">{props.curtidas}</strong>
+                        Curtido por <strong>{props.amigo}</strong> e <strong data-test="likes-number">outras {totalLikes.toLocaleString("pt-br")} pessoas</strong>
                     </div>
                 </div>
             </div>
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,30 +1,46 @@
 import React, {useState} from "react";
 
 export default function Usuario(){
-    const imagemPadrao = "assets/img/catanacomics.svg"
-    const usuarioPadrao = "Catana"
-    const [usuario, setUsuario] = useState(usuarioPadrao);
-    const [imagem, setImagem] = useState(imagemPadrao)
-
-    function mudarNome() {
-        setUsuario(prompt('Qual o seu nome?'))
-    }
-
-    function mudarImagem() {
-        const imagemAdicionada = prompt('Insira o link da sua imagem')
-        setImagem(imagemAdicionada)
-    }
+    
+    const userData = [
+        {
+            usuario: "catanacomics",
+            nome: "Catana",
+            imagem: "assets/img/catanacomics.svg"
+        }
+    ]
 
     return (
+        <div class="usuario">
+            {userData.map((user) => <User usuario={user.usuario} nome={user.nome} imagem={user.imagem}/>)}
+        </div>
+    )
+
+function User(props){
+    const [nome, setNome] = useState(props.nome);
+    const [imagem, setImagem] = useState(props.imagem)
+    
+    return (
         <div data-test="user" class="usuario">
-            <img data-test="profile-image" onClick={mudarImagem} src={!imagem ? imagemPadrao : imagem} />
+            <img data-test="profile-image" onClick={mudarImagem} src={!imagem ? props.imagem : imagem} />
             <div class="texto">
-                <strong>catanacomics</strong>
+                <strong>{props.usuario}</strong>
                 <span data-test="name">
-                    {!usuario ? usuarioPadrao : usuario}
+                    {!nome ? props.nome : nome}
                     <ion-icon data-test="edit-name" onClick={mudarNome} name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
     )
+
+    function mudarNome() {
+        setNome(prompt('Qual o seu nome?'))
+    }
+    
+    function mudarImagem() {
+        const imagemAdicionada = prompt('Insira o link da sua imagem')
+        setImagem(imagemAdicionada)
+    }
+}
+
 }
